a94aa1b0f8b480f7365833ec32619b9dca127fdc
leon19019 <leon18018@yandex.ru>
2021-02-15 10:08:31 +0300

    Now it is my code

--- game_engine/tic_tac_toe_game.py
+++ game_engine/tic_tac_toe_game.py
@@ -4,53 +4,74 @@ from .tic_tac_toe_common_lib import TicTacToeTurn, TicTacToeGameInfo, AbstractTi
 
 
 class TicTacToeGame(AbstractTicTacToeGame):
-    """Наследуемся от абстрактного класса и реализуем ручками все методы"""
+    def __init__(
+        self,
+        game_id,
+        first_player_id,
+        second_player_id
+    ):
+        self._player_id = first_player_id
+        self.TicTacToeGameInfo = TicTacToeGameInfo(
+            game_id = game_id,
+            field = [
+            [" ", " ", " "],
+            [" ", " ", " "],
+            [" ", " ", " "]
+            ],
+            sequence_of_turns = [],
+            first_player_id = first_player_id,
+            second_player_id = second_player_id,
+            winner_id = ""
+        )
 
-    def __init__(self, game_id: str, first_player_id: str, second_player_id: str,
-                 strategy: Optional[Callable[[TicTacToeGameInfo], TicTacToeTurn]] = None) -> None:
-        self._game_id = game_id
-        self._first_player_id = first_player_id
-        self._second_player_id = second_player_id
-        self._winner_id = ""
-        self._strategy = strategy
-        self._turns: List[TicTacToeTurn] = []
+    def is_turn_correct(self, TicTacToeTurn):
+        X = TicTacToeTurn.x_coordinate
+        Y = TicTacToeTurn.y_coordinate
 
-    def is_turn_correct(self, turn: TicTacToeTurn) -> bool:
-        if self._winner_id != "":
+        if self.TicTacToeGameInfo.winner_id == "":
+            if self._player_id == TicTacToeTurn.player_id:
+                if (X >= 0 and X < 3) and (Y >= 0 and Y < 3):
+                    if self.TicTacToeGameInfo.field[X][Y] == " ":
+                        return True
+                    else:
+                        return False
+                else:
+                    return False
+            else:
+                return False
+        else:
             return False
 
-        if self.current_player_id() != turn.player_id:
-            return False
 
-        if not (0 <= turn.x_coordinate <= 2 and 0 <= turn.y_coordinate <= 2):
-            return False
+    def do_turn(self, TicTacToeTurn):
+        X = TicTacToeTurn.x_coordinate
+        Y = TicTacToeTurn.y_coordinate
 
-        return self.get_game_info().field[turn.x_coordinate][turn.y_coordinate] == " "
+        if self.is_turn_correct(TicTacToeTurn):
+            if TicTacToeTurn.player_id == self.TicTacToeGameInfo.first_player_id:
+                self.TicTacToeGameInfo.field[X][Y] = "X"
+                self._player_id = self.TicTacToeGameInfo.second_player_id
+            
+            elif TicTacToeTurn.player_id == self.TicTacToeGameInfo.second_player_id:
+                self.TicTacToeGameInfo.field[X][Y] = "O"
+                self._player_id = self.TicTacToeGameInfo.first_player_id
 
-    def current_player_id(self) -> str:
-        if not self._turns:
-            return self._first_player_id
 
-        if self._turns[-1].player_id == self._first_player_id:
-            return self._second_player_id
 
-        return self._first_player_id
+            self.TicTacToeGameInfo.sequence_of_turns.append(TicTacToeTurn)
 
-    def do_turn(self, turn: TicTacToeTurn) -> TicTacToeGameInfo:
-        if not self.is_turn_correct(turn):
-            return self.get_game_info()
-
-        self._turns.append(deepcopy(turn))
-        self._set_winner()
-
-        if self._winner_id == "" and self._strategy is not None:
-            self._turns.append(self._strategy(self.get_game_info()))
             self._set_winner()
 
-        return self.get_game_info()
+            return self.TicTacToeGameInfo
+        else:
+            return self.TicTacToeGameInfo
+
+    
+    def get_game_info(self):
+        return deepcopy(self.TicTacToeGameInfo)
 
     def _set_winner(self) -> None:
-        if self._winner_id != "":
+        if self.TicTacToeGameInfo.winner_id != "":
             return
 
         field = self.get_game_info().field
@@ -64,11 +85,11 @@ class TicTacToeGame(AbstractTicTacToeGame):
                 row2.append(field[j][i])
 
             if row1 == ["X"] * 3 or row2 == ["X"] * 3:
-                self._winner_id = self._first_player_id
+                self._winner_id = self.TicTacToeGameInfo.first_player_id
                 return
 
             if row1 == ["O"] * 3 or row2 == ["O"] * 3:
-                self._winner_id = self._second_player_id
+                self._winner_id = self.TicTacToeGameInfo.second_player_id
                 return
 
             if ("X" not in row1) or ("X" not in row2) or ("O" not in row1) or ("O" not in row2):
@@ -83,11 +104,11 @@ class TicTacToeGame(AbstractTicTacToeGame):
             row2.append(field[i][2 - i])
 
         if row1 == ["X"] * 3 or row2 == ["X"] * 3:
-            self._winner_id = self._first_player_id
+            self._winner_id = self.TicTacToeGameInfo.first_player_id
             return
 
         if row1 == ["O"] * 3 or row2 == ["O"] * 3:
-            self._winner_id = self._second_player_id
+            self._winner_id = self.TicTacToeGameInfo.second_player_id
             return
 
         if ("X" not in row1) or ("X" not in row2) or ("O" not in row1) or ("O" not in row2):
@@ -95,24 +116,3 @@ class TicTacToeGame(AbstractTicTacToeGame):
 
         if draw:
             self._winner_id = "draw"
-
-    def get_game_info(self) -> TicTacToeGameInfo:
-        result = TicTacToeGameInfo(
-            game_id=self._game_id,
-            field=[
-                [" ", " ", " "],
-                [" ", " ", " "],
-                [" ", " ", " "]
-            ],
-            sequence_of_turns=deepcopy(self._turns),
-            first_player_id=self._first_player_id,
-            second_player_id=self._second_player_id,
-            winner_id=self._winner_id
-        )
-        for turn in self._turns:
-            if turn.player_id == self._first_player_id:
-                symb = "X"
-            else:
-                symb = "O"
-            result.field[turn.x_coordinate][turn.y_coordinate] = symb
-        return result
